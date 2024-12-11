ada2e4987cf3fec7c001f1d8e187a2874f22f4b3
PieStw <piero.stalteri@icloud.com>
2024-12-11 14:37:28 +0100

    fetch data

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import style from "./Card.module.css";
+
+export default function Card({ element }) {
+  return (
+    <div className="list">
+      <div className="item">
+        <div className={`${style.card}`}>
+          <img src={element.img} className="card-img-top" />
+          <div className="card-body">
+            <h2 className="card-title">{element.name}</h2>
+            <h5 className="card-subtitle ">{element.author}</h5>
+            <h5 className="card-subtitle mt-1">{element.state}</h5>
+            <p className="card-text">{element.content}</p>
+            <p>Tags:</p>
+
+            {element.tags[0] && (
+              <span className="badge text-bg-primary me-2">HTML</span>
+            )}
+            {element.tags[1] && (
+              <span className="badge text-bg-success me-2">CSS</span>
+            )}
+            {element.tags[2] && (
+              <span className="badge text-bg-info me-2">JS</span>
+            )}
+
+            <hr />
+
+            <button
+              className="btn btn-danger me-2"
+              onClick={() => {
+                deleteArticle(index);
+              }}
+            >
+              <i className="fa-solid fa-trash"></i>
+            </button>
+            <button
+              className="btn btn-primary me-2"
+              onClick={() => {
+                editArticle(index);
+              }}
+            >
+              <i class="fa-solid fa-chevron-right"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar";
+import { useState, useEffect } from "react";
+import Card from "../components/card/Card";
+import style from "./PostPage.module.css";
 
 export default function PostPage() {
+  const [articleList, setArticleList] = useState([]);
+
+  function fetchAllArticle() {
+    fetch("http://localhost:3000/post")
+      .then((res) => res.json())
+      .then((data) => setArticleList(data.posts));
+  }
+
+  useEffect(() => {
+    fetchAllArticle();
+  }, []);
+
   return (
     <>
-      <h1>PostPage</h1>
+      <div className={`${style.list}`}>
+        {articleList.map((element, index) => (
+          <Card key={index} element={element}></Card>
+        ))}
+      </div>
     </>
   );
 }
